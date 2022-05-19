ab9a0cca17c10ced657ea4cf795f76e31c9f2126
Giang <463189@student.fontys.nl>
2022-05-19 12:09:46 +0200

    Add dialogue

---
+++ Assets/Scripts/Dialogue/DialogueUI.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using TMPro;
+public class DialogueUI : MonoBehaviour
+{
+    [SerializeField] private GameObject dialogueBox;
+    [SerializeField] private TMP_Text textLabel;
+    [SerializeField] private DialogueObject testDialogue;
+    
+    private TypeWriterEffect typeWriterEffect;
+    private void Start() {
+        typeWriterEffect = GetComponent<TypeWriterEffect>();
+        closeDialogueBox();
+        showDialogue(testDialogue);
+    }
+ 
+    public void showDialogue(DialogueObject dialogueObject) {
+        dialogueBox.SetActive(true);
+        StartCoroutine(stepThroughDialogue(dialogueObject));
+    }
+
+    private IEnumerator stepThroughDialogue(DialogueObject dialogueObject) {
+        foreach (string dialogue in dialogueObject.Dialogue) {
+            yield return runTypingEffect(dialogue);
+            textLabel.text = dialogue;
+            yield return null;
+            yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
+        }
+        closeDialogueBox();
+    }
+
+    private IEnumerator runTypingEffect(string dialogue) {
+        typeWriterEffect.run(dialogue, textLabel);
+        while (typeWriterEffect.isRunning) {
+            yield return null;
+            if (Input.GetKeyDown(KeyCode.Space)) {
+                typeWriterEffect.Stop();
+            }
+        }
+    }
+    
+    private void closeDialogueBox() {
+        dialogueBox.SetActive(false);
+        textLabel.text = string.Empty;
+    }
+}
+ 
