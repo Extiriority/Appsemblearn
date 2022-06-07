86c1c53d4d1a82714a76be1a2787ce41244f2c92
Tim de Moor <timdemoor122@gmail.com>
2022-06-07 06:21:53 +0200

    Fix key generator

--- Assets/Scripts/PirateMiniGame/Key.cs
+++ Assets/Scripts/PirateMiniGame/Key.cs
@@ -4,41 +4,49 @@ using System.Collections.Generic;
 using System.Linq;
 using PirateMiniGame;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Key : MonoBehaviour
 {
-    [SerializeField] private int keyLayout = 11111;
+    [Range(10000f, 99999f)][SerializeField] private int keyLayout;
     private List<KeyTooth> _teeth;
+
+    [SerializeField] private bool test;
     void Start()
     {
         SetVariables();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void SetKeyLayout()
     {
         int i = 0;
+        int j = 0;
         foreach (KeyTooth t in _teeth)
         {
-            t.SetKeyValue(keyLayout.ToString()[i]);
-            print(keyLayout.ToString()[i]);
+            t.SetToothValue(keyLayout.ToString()[i]);
+            t.SetToothIndex(j);
             i++;
+            j++;
         }
     }
 
     private void OnValidate()
     {
         SetVariables();
+        SetRandomCode();
         SetKeyLayout();
+        test = false;
     }
 
     private void SetVariables()
     {
         _teeth = GetComponentsInChildren<KeyTooth>().ToList();
+        
+    }
+
+    private void SetRandomCode()
+    {
+        keyLayout = Random.Range(10000, 99999);
     }
 }
--- Assets/Scripts/PirateMiniGame/KeyTooth.cs
+++ Assets/Scripts/PirateMiniGame/KeyTooth.cs
@@ -1,15 +1,28 @@
+using System;
 using UnityEngine;
 
 namespace PirateMiniGame
 {
     public class KeyTooth : MonoBehaviour
     {
-        [Range(1, 5)] private int _toothValue = 5;
-
-        public void SetKeyValue(int newValue)
+        private int _value = 1;
+        private int _index = 1;
+        
+        public void SetToothValue(int newValue)
         {
-            _toothValue = newValue;
-            transform.localPosition = new Vector3(_toothValue, 0f, 0f);
+            _value = newValue;
+            SetPosition();
+        }
+
+        public void SetToothIndex(int newIndex)
+        {
+            _index = newIndex;
+            SetPosition();
+        }
+
+        private void SetPosition()
+        {
+            transform.localPosition = new Vector3(_value*.1f, 0f, _index*.5f);
         }
     }
 }
