b24757da1bff258e36fe58824dd4b44567fd0d4c
Tim de Moor <timdemoor122@gmail.com>
2022-06-17 10:27:18 +0200

    Add animation to key

--- Assets/EnemySpawn.cs
+++ Assets/EnemySpawn.cs
@@ -8,39 +8,25 @@ public class EnemySpawn : MonoBehaviour
     public GameObject enemyShipPrefab;
     public GameObject enemyShip2Prefab;
 
-    Transform canvasTransform;
+    private Transform canvasTransform;
     public GameObject canvas;
 
     [SerializeField] public GameObject lockPickingCanvas;
 
     public int liveShipCount = 0;
-
-
-    // Start is called before the first frame update
+    
     void Start()
     {
         canvasTransform = canvas.transform;
         spawn();
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-       
     }
 
     void spawn()
     {
-       
-
-
         if (liveShipCount < 3)
         {
-
             GameObject tempShip = new GameObject();
             int selectShip = Random.Range(1, 3);
-            print(selectShip);
 
             switch (selectShip)
             {
@@ -56,15 +42,11 @@ public class EnemySpawn : MonoBehaviour
             liveShipCount++;
 
             GameObject ship = Instantiate(tempShip, spawnPoint.position, spawnPoint.rotation);
-
             ship.GetComponent<EnemyHealth>().enemySpawn = this;
-
             ship.GetComponent<GettingBoarded>().lockPickingCanvas = lockPickingCanvas;
-
             ship.transform.SetParent(canvasTransform);
         }
         
-
         Invoke("spawn", 15);
     }
 }
