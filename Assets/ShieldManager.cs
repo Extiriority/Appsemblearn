f3da741a5222bc5a8da921fc66a4c38c6a453277
Giang <463189@student.fontys.nl>
2022-06-23 12:39:40 +0200

    Fix folder structure

--- Assets/ShieldManager.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ShieldManager : MonoBehaviour
-{
-    public Color currentColor;
-    public GameObject currentIcon;
-}
